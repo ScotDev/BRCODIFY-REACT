9b4f848a32251b9a2e9abe6d1ba910942822f6d8
Callum <callum.git@outlook.com>
2020-06-06 21:57:26 +0100

    Simplified file structure, began adding error handling

--- src/components/Functional/Input.js
+++ src/components/Functional/Input.js
@@ -22,9 +22,7 @@ export default class Input extends Component {
         const url = "about:blank";
         const newWindow = window.open(url, "_new");
         newWindow.document.open();
-        newWindow.document.write(`<html><head><script>function step1(){\n +
-        setTimeout('step2()', 10);}
-        </script></head><body onload='step1()'> 
+        newWindow.document.write(`<html><head><<script>function step1(){setTimeout('step2()', 10);}</script></head><body onload='step1()'> 
         <img src='${img}' /></body></html>`);
         newWindow.print();
         console.log('Barcode printed', this.state.barcodeValue);
