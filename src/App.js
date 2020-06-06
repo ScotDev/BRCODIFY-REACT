6a370c483705e8458fa58bb611779cb99eba5e9f
Callum <callum.git@outlook.com>
2020-06-06 21:54:45 +0100

    Simplified file structure, began adding error handling

--- src/App.js
+++ src/App.js
@@ -2,29 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Input from './components/Functional/Input'
-// import Barcode from './components/Functional/Barcode';
 import Credits from './components/Layout/Credits';
 
 
 export default class App extends Component {
 
-  state = { BarcodeExists: false, input: 'Example code 12345' }
-
-  updateAppState = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-  // passData = (BarcodeExists, input) => {
-  //   this.setState
-  // }
-
+  state = { format: 'CODE128' }
 
   render() {
     return (
       <div className="App container" >
         <h1>BRCODIFY</h1>
         <h2>A simple barcode generator</h2>
-        <h3>Format: CODE128</h3>
-        <Input value={this.state.value} updateAppState={this.updateAppState}></Input>
+        <h3>Format: {this.state.format}</h3>
+        <Input></Input>
         <Credits></Credits>
       </div>
     )
--- src/components/Functional/Input.js
+++ src/components/Functional/Input.js
@@ -1,33 +1,80 @@
-import React, { Component } from 'react'
-
-import Barcode from './Barcode'
+import React, { Component } from 'react';
+import JsBarcode from 'jsbarcode'
 
 export default class Input extends Component {
 
-    state = { BarcodeExists: false, input: 'Example code 12345' }
+    state = { format: 'CODE128', BarcodeExists: false, input: 'Example code 12345', barcodeValue: 'Example code 12345', showWarning: false, errorMsg: 'No error' }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.setState({ [e.target.name]: e.target.value, BarcodeExists: true })
+
+
+    generateBarcode = (val, format) => {
+        JsBarcode('#barcode', val, { format: format });
     }
 
+    componentDidMount() {
+        this.generateBarcode(this.state.barcodeValue, 'CODE128');
+        console.log(this.state.barcodeValue);
+    }
+
+    printBarcode = () => {
+        const canvas = document.getElementById('barcode');
+        const img = canvas.toDataURL('image/png');
+        const url = "about:blank";
+        const newWindow = window.open(url, "_new");
+        newWindow.document.open();
+        newWindow.document.write(`<html><head><script>function step1(){\n +
+        setTimeout('step2()', 10);}
+        </script></head><body onload='step1()'> 
+        <img src='${img}' /></body></html>`);
+        newWindow.print();
+        console.log('Barcode printed', this.state.barcodeValue);
+    }
+
+
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const defaultValue = 'Example code 12345';
+        if (e.target.input.value.length > 50) {
+            this.setState({ BarcodeExists: false, barcodeValue: this.state.input, input: defaultValue, showWarning: true, errorMsg: 'Code cannot be longer than 50 characters' })
+        } else if (e.target.input.value.startsWith(' ')) {
+            this.setState({ BarcodeExists: false, barcodeValue: this.state.input, input: defaultValue, showWarning: true, errorMsg: 'Code cannot start with a blank space' })
+        } else {
+            this.setState({ BarcodeExists: true, barcodeValue: this.state.input, input: e.target.input.value, showWarning: false })
+            this.generateBarcode(this.state.input, this.state.format);
+        }
+    }
+
+    handleChange = e => {
+        const defaultValue = 'Example code 12345';
+        if (e.target.value.length < 1) {
+            this.setState({ barcodeValue: defaultValue, BarcodeExists: false, input: defaultValue, showWarning: false, errorMsg: 'Code must contain at least 1 character' })
+        } else if (e.target.value.startsWith(' ')) {
+            this.setState({ barcodeValue: defaultValue, BarcodeExists: false, input: defaultValue, showWarning: true, errorMsg: 'Code cannot start with a blank space' })
+        } else {
+            this.setState({ barcodeValue: this.state.input, BarcodeExists: true, input: e.target.value })
+        }
+    }
+
+
     render() {
-        const { updateAppState } = this.props;
+        const { showWarning, errorMsg } = this.state;
         return (
             <>
                 <form onSubmit={this.handleSubmit}>
                     <input
                         type="text"
-                        id="input"
                         name="input"
-                        defaultValue="Example code 12345"
+                        placeholder="Example code 12345"
                         autoFocus
                         autoComplete="off"
-                        onChange={updateAppState}
+                        onChange={this.handleChange}
                     />
-                    <button className="btn" id="btn" >Generate barcode</button>
+                    {showWarning && (<div id="warning" className="warning">{errorMsg}</div>)}
+                    <button className="btn">Generate barcode</button>
                 </form>
-                <Barcode barcodeData={this.state}></Barcode>
+                <canvas id="barcode"></canvas>
+                <button className="btn" id="PrintBtn" onClick={this.printBarcode}>Print barcode</button>
             </>
         )
     }
